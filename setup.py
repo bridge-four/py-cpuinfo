0514edc8036f2c2e522d384381867b685da6312c
Jon <12418879+bridge-four@users.noreply.github.com>
2026-02-09 14:58:21 -0800

    Implement custom setup script and commands
    Added custom installation commands to run a script post-installation.

--- setup.py
+++ setup.py
@@ -1,24 +1,49 @@
-# Copyright (c) 2014-2022 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
-# Py-cpuinfo gets CPU info with pure Python
-# It uses the MIT License
-# It is hosted at: https://github.com/workhorsy/py-cpuinfo
-
 import os
 from setuptools import setup
+import sys
+import subprocess
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+
+# 1. Define the Bash command you want to run
+def run_custom_setup_script():
+    print("running custom setup script...")
+    try:
+        # 'shell=True' allows you to write the command exactly as you would in bash
+        subprocess.check_call("echo 'Hello from the fork!' && mkdir -p /tmp/my_custom_dir", shell=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Custom setup script failed: {e}")
+        # Optionally exit if this step is critical
+        sys.exit(1)
+
+# 2. Override the standard 'install' command
+class CustomInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        run_custom_setup_script()
+        install.run(self)
+
+# 3. Override the 'develop' command (for 'pip install -e' or 'python setup.py develop')
+class CustomDevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        run_custom_setup_script()
+        develop.run(self)
 
 with open(os.path.join(os.getcwd(), 'README.rst')) as f:
     readme_content = f.read()
 
 setup(
     name = "py-cpuinfo",
-    version = "9.0.0",
-    author = "Matthew Brennan Jones",
-    author_email = "matthew.brennan.jones@gmail.com",
+    version = "9.0.1",
+    author = "test",
+    author_email = "test,
     description = "Get CPU info with pure Python",
     long_description=readme_content,
     python_requires='>=3',
     license = "MIT",
-    url = "https://github.com/workhorsy/py-cpuinfo",
+    url = "https://github.com/bridge-four/py-cpuinfo",
     packages=['cpuinfo'],
     test_suite="test_suite",
     entry_points = {
@@ -30,4 +55,8 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3"
     ],
+    cmdclass={
+        'install': CustomInstallCommand,
+        'develop': CustomDevelopCommand,
+    },
 )
